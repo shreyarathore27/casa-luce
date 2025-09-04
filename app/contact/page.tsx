96e8f51908d74b210aa37f457d7d49c40c627678
Shreya Rathore <shreyawork0709@gmail.com>
2025-09-04 20:33:40 +0530

    minute changes

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Contact Page</h1>
+    </div>
+  );
+};
+
+export default Contact;
